a0df683b81a75239d7a143ab42271d1978c8c8b6
Dan Shearer <nickelaway@users.noreply.github.com>
2026-02-20 11:17:31 +0000

    Fix some of the checkstyle warnings.

--- aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscriptionException.java
+++ aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscriptionException.java
@@ -1,22 +1,62 @@
+/*
+ * Copyright 2014-2025 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.aeron.archive.client;
 
+/**
+ * Exception raised when using a {@link PersistentSubscription}.
+ */
 public class PersistentSubscriptionException extends RuntimeException
 {
     private static final long serialVersionUID = -6953277621087533657L;
 
     private final Reason reason;
 
-    public enum Reason {
+    /**
+     * Reason a {@link PersistentSubscriptionException} occurred.
+     */
+    public enum Reason
+    {
+        /**
+         * No recording exists with the specified recording ID.
+         */
         RECORDING_NOT_FOUND,
+        /**
+         * The requested start position is not available for the specified recording.
+         */
         INVALID_START_POSITION
     }
 
+    /**
+     * Persistent Subscription exception with a detailed message and provided reason.
+     *
+     * @param reason  for the error.
+     * @param message providing detail on the error.
+     */
     public PersistentSubscriptionException(final Reason reason, final String message)
     {
         super(message);
         this.reason = reason;
     }
 
+    /**
+     * Returns the reason indicating the type of error that caused the exception.
+     *
+     * @return reason
+     */
     public Reason reason()
     {
         return reason;
