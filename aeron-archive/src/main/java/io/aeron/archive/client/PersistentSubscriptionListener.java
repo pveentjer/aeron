d49b3ef1f365e863c177d13281fb812b5ae88bd2
Dan Shearer <nickelaway@users.noreply.github.com>
2026-02-20 11:17:31 +0000

    Fix some of the checkstyle warnings.

--- aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscriptionListener.java
+++ aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscriptionListener.java
@@ -1,8 +1,38 @@
+/*
+ * Copyright 2014-2025 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.aeron.archive.client;
 
+/**
+ * Interface for delivering notifications about changes in state of a {@link PersistentSubscription}.
+ */
 public interface PersistentSubscriptionListener
 {
+    /**
+     * Called when the {@link PersistentSubscription} transitions to consuming from the live channel.
+     */
     void onLive();
 
-    void onError(final Exception e);
+    /**
+     * Called when the {@link PersistentSubscription} encounters an error.
+     * <p>
+     * This will be called for both terminal and non-terminal errors.
+     *
+     * @param e the exception that caused the error.
+     * @see PersistentSubscription#hasFailed()
+     */
+    void onError(Exception e);
 }
