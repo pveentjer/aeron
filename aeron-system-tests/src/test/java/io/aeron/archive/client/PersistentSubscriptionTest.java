97f80de90b681fd769ec2832cd8f6cd5b19e6d8c
Wojciech Lukowicz <wojciech.lukowicz@weareadaptive.com>
2026-02-24 11:09:09 +0000

    remove unused import

--- aeron-system-tests/src/test/java/io/aeron/archive/client/PersistentSubscriptionTest.java
+++ aeron-system-tests/src/test/java/io/aeron/archive/client/PersistentSubscriptionTest.java
@@ -58,7 +58,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
