6ca436b654b515d2baafbf1a9baa04a8204893d9
Wojciech Lukowicz <wojciech.lukowicz@weareadaptive.com>
2026-04-13 11:27:44 +0100

    update copyright headers

--- aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscriptionListener.java
+++ aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscriptionListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2025 Real Logic Limited.
+ * Copyright 2026 Adaptive Financial Consulting Limited.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
