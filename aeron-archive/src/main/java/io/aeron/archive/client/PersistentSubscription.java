c934623c905e13d28902b9e0f29267fab4c6c21f
Dan Shearer <nickelaway@users.noreply.github.com>
2026-04-20 12:00:40 +0100

    Update comparisons with constants to match the convention elsewhere in Aeron

--- aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscription.java
+++ aeron-archive/src/main/java/io/aeron/archive/client/PersistentSubscription.java
@@ -269,7 +269,7 @@ public final class PersistentSubscription implements AutoCloseable
      */
     public boolean isLive()
     {
-        return state == State.LIVE;
+        return State.LIVE == state;
     }
 
     /**
@@ -279,7 +279,7 @@ public final class PersistentSubscription implements AutoCloseable
      */
     public boolean isReplaying()
     {
-        return state == State.REPLAY || state == State.ATTEMPT_SWITCH;
+        return State.REPLAY == state || State.ATTEMPT_SWITCH == state;
     }
 
     /**
@@ -293,7 +293,7 @@ public final class PersistentSubscription implements AutoCloseable
      */
     public boolean hasFailed()
     {
-        return state == State.FAILED;
+        return State.FAILED == state;
     }
 
     /**
